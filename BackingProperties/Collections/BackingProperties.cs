40a85da672f0725f7bbe0a73988ac107306b0739
Alex Tselevich <atselevich@gmail.com>
2015-05-20 17:12:49 -0400

    initial commit

---
+++ BackingProperties/Collections/BackingProperties.cs
@@ -0,0 +1,176 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="BackingProperties.cs" company="">
+//   
+// </copyright>
+// <summary>
+//   The module settings base.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace BackingProperties
+{
+    using System;
+    using System.Collections.Concurrent;
+    using System.Collections.Generic;
+    using System.Runtime.CompilerServices;
+
+    /// <summary>
+    ///     The module settings base.
+    /// </summary>
+    public class BackingProperties : ConcurrentDictionary<string, object>
+    {
+        #region Public Methods and Operators
+
+        /// <summary>
+        /// The get cached value.
+        /// </summary>
+        /// <param name="method">
+        /// The method.
+        /// </param>
+        /// <param name="propertyName">
+        /// The property name.
+        /// </param>
+        /// <typeparam name="T">
+        /// The object
+        /// </typeparam>
+        /// <returns>
+        /// The <see cref="T"/>.
+        /// </returns>
+        public T GetPropertyValue<T>(Func<T> method, [CallerMemberName] string propertyName = null)
+        {
+            if (this.ContainsKey(propertyName))
+            {
+                // ReSharper disable once ExplicitCallerInfoArgument
+                var value = this.GetPropertyValue<T>(propertyName);
+
+                return value;
+            }
+
+            var newValue = method();
+            this.SetPropertyValue(newValue, propertyName);
+
+            return newValue;
+        }
+
+        /// <summary>
+        /// The get property value.
+        /// </summary>
+        /// <param name="propertyName">
+        /// The property name.
+        /// </param>
+        /// <typeparam name="T">
+        /// The object
+        /// </typeparam>
+        /// <returns>
+        /// The <see cref="T"/>.
+        /// </returns>
+        /// <exception cref="ArgumentNullException">
+        /// Thrown if argument is null
+        /// </exception>
+        public T GetPropertyValue<T>([CallerMemberName] string propertyName = null)
+        {
+            if (propertyName == null)
+            {
+                throw new ArgumentNullException("propertyName");
+            }
+
+            object value;
+            if (this.TryGetValue(propertyName, out value))
+            {
+                return (T)value;
+            }
+
+            return default(T);
+        }
+
+        /// <summary>
+        /// The get cached value.
+        /// </summary>
+        /// <param name="method">
+        /// The method.
+        /// </param>
+        /// <param name="defaultValue">
+        /// The default Value.
+        /// </param>
+        /// <param name="propertyName">
+        /// The property name.
+        /// </param>
+        /// <typeparam name="T">
+        /// The object
+        /// </typeparam>
+        /// <returns>
+        /// The <see cref="T"/>.
+        /// </returns>
+        public T GetPropertyValueOrDefault<T>(
+            Func<T> method, 
+            T defaultValue, 
+            [CallerMemberName] string propertyName = null)
+        {
+            // ReSharper disable ExplicitCallerInfoArgument
+            if (this.ContainsKey(propertyName))
+            {
+                var value = this.GetPropertyValue<T>(propertyName);
+                return value;
+            }
+
+            var newValue = method();
+            if (newValue == null)
+            {
+                return defaultValue;
+            }
+
+            this.SetPropertyValue(newValue, propertyName);
+
+            return newValue;
+        }
+
+        /// <summary>
+        /// The set property value.
+        /// </summary>
+        /// <param name="newValue">
+        /// The new value.
+        /// </param>
+        /// <param name="propertyName">
+        /// The property name.
+        /// </param>
+        /// <typeparam name="T">
+        /// The object
+        /// </typeparam>
+        /// <exception cref="ArgumentNullException">
+        /// Thrown if argument is null
+        /// </exception>
+        /// <returns>
+        /// The <see cref="T"/>.
+        /// </returns>
+        public T SetPropertyValue<T>(T newValue, [CallerMemberName] string propertyName = null)
+        {
+            if (propertyName == null)
+            {
+                throw new ArgumentNullException("propertyName");
+            }
+
+            if (!EqualityComparer<T>.Default.Equals(newValue, this.GetPropertyValue<T>(propertyName)))
+            {
+                this[propertyName] = newValue;
+            }
+
+            return newValue;
+        }
+
+        /// <summary>
+        /// The remove.
+        /// </summary>
+        /// <param name="key">
+        /// The key.
+        /// </param>
+        /// <returns>
+        /// The <see cref="bool"/>.
+        /// </returns>
+        public bool Remove(string key)
+        {
+            return ((IDictionary<string, object>)this).Remove(key);
+        }
+
+        #endregion
+    }
+}
---
+++ BackingPropertiesConsoleDemo/Program.cs
@@ -0,0 +1,161 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Program.cs" company="">
+//   
+// </copyright>
+// <summary>
+//   The program.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace BackingPropertiesConsoleDemo
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+
+    using BackingProperties;
+
+    /// <summary>
+    /// The program.
+    /// </summary>
+    internal class Program
+    {
+        #region Static Fields
+
+        /// <summary>
+        ///     The backing properties.
+        /// </summary>
+        private static readonly BackingProperties BackingProperties = new BackingProperties();
+
+        #endregion
+
+        #region Public Properties
+
+        // Method Group
+        /// <summary>
+        ///     Gets the file count.
+        /// </summary>
+        public static int NovemberDays
+        {
+            get
+            {
+                return BackingProperties.GetPropertyValue(GetDaysInNovember);
+            }
+        }
+
+        // Method Call
+        /// <summary>
+        ///     Gets the file count times ticks.
+        /// </summary>
+        public static long NovermberDaysTimesTicks
+        {
+            get
+            {
+                return BackingProperties.GetPropertyValue(() => Multiply(Ticks, NovemberDays));
+            }
+        }
+
+        // Anonymous Method
+        /// <summary>
+        ///     Gets the ticks.
+        /// </summary>
+        public static long Ticks
+        {
+            get
+            {
+                return BackingProperties.GetPropertyValue(
+                    () =>
+                        {
+                            var number = DateTime.Now.Ticks;
+                            return number;
+                        });
+            }
+        }
+
+        // Constant Value
+        /// <summary>
+        ///     Gets the written in.
+        /// </summary>
+        public static string WrittenIn
+        {
+            get
+            {
+                return BackingProperties.GetPropertyValue(() => "This was written in C#");
+            }
+        }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        ///     The get file count on c drive.
+        /// </summary>
+        /// <returns>
+        ///     The <see cref="int" />.
+        /// </returns>
+        private static int GetDaysInNovember()
+        {
+            return DateTime.DaysInMonth(2015, 11);
+        }
+
+        /// <summary>
+        /// The main.
+        /// </summary>
+        /// <param name="args">
+        /// The args.
+        /// </param>
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Ticks {0}", Ticks);
+            Console.WriteLine("NovemberDays {0}", NovemberDays);
+            Console.WriteLine("NovermberDaysTimesTicks {0}", NovermberDaysTimesTicks);
+
+            BackingProperties.SetPropertyValue(20, "NovemberDays");
+            Console.WriteLine("We just set NovemberDays to 20, did it work?");
+            Console.WriteLine("Retrieving NovemberDays With Cast To Int");
+            Console.WriteLine("NovemberDays {0}", BackingProperties.GetPropertyValue<int>("NovemberDays"));
+            Console.WriteLine("Retrieving NovemberDays By Accessing Property");
+            Console.WriteLine("NovemberDays {0}", NovemberDays);
+            Console.WriteLine("NovermberDaysTimesTicks {0}", NovermberDaysTimesTicks);
+
+            Console.WriteLine("If I need to run the logic that usually gets NovemberDays I need remove the backing property");
+
+            BackingProperties.Remove("NovemberDays");
+
+            Console.WriteLine("Now the logic will fire again because the Backing Property is clear");
+            Console.WriteLine("NovemberDays {0}", NovemberDays);
+            Console.WriteLine("NovermberDaysTimesTicks {0}", NovermberDaysTimesTicks);
+
+            Console.WriteLine(WrittenIn);
+            Console.Read();
+        }
+
+        /// <summary>
+        /// The multiply.
+        /// </summary>
+        /// <param name="ticks">
+        /// The ticks.
+        /// </param>
+        /// <param name="fileCount">
+        /// The file count.
+        /// </param>
+        /// <returns>
+        /// The <see cref="long"/>.
+        /// </returns>
+        private static long Multiply(long ticks, int fileCount)
+        {
+            return ticks * fileCount;
+        }
+
+        public class Car
+        {
+            public string Make { get; set; }
+
+            public string Color { get; set; }
+        }
+
+        #endregion
+    }
+}
